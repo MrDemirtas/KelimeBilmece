7bc04408e145e65eb509ae10ae99a681b1bb6abc
Furkan Demirtaş <furkan-demirtas@outlook.com>
2024-11-05 19:00:21 +0300

    Bug fixed and comments added

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,11 +7,12 @@ const statusMsgContentStatus = document.querySelector(".statusMsgContentStatus")
 const playAgain = document.querySelector(".playAgain");
 const tryAgain = document.querySelector(".tryAgain");
 
-let word;
-let wordArray = [];
-let getWordCount = [];
-let wordsDB = [];
+let word; // * Rastgele seçilen kelime. örn: "SELAM"
+let wordArray = []; // * Rastgele seçilen kelimenin harf harf diziye aktarılması. örn: ["S", "E", "L", "A", "M"]
+let getWordCount = []; // * Rastgele harf alımı için kelime uzunluğunu diziye aktarılması. örn: [0, 1, 2, 3, 4] => 0'ıncı eleman "S" harfini temsil ediyor. 1 = "E", 2 = "L" ...
+let wordsDB = []; // * Kelime havuzu
 
+// * Kelime sayısı kadar input elementi ekleme işlemi
 const addInput = async () => {
   wordArray.forEach((element) => {
     inputWordContent.innerHTML += `<input type="text" class="inputWord" maxlength="1" onfocus="this.select();">`;
@@ -19,30 +20,36 @@ const addInput = async () => {
   return;
 };
 
+// * Rastgele kelime seçimi ve input etkileşimleri
 const loadWord = () => {
-  inputWordContent.innerHTML = "";
-  tdk.innerHTML = "";
-  word = wordsDB[Math.floor(Math.random() * wordsDB.length)].toLocaleUpperCase("tr-TR");
-  wordArray = Array.from(word);
-  wordNumber.innerHTML = wordArray.length;
+  inputWordContent.innerHTML = ""; // * Sıfırlama
+  tdk.innerHTML = ""; // * Sıfırlama
+  getWordCount = []; // * Sıfırlama
+  word = wordsDB[Math.floor(Math.random() * wordsDB.length)].toLocaleUpperCase("tr-TR"); // * Kelime havuzundan rasgele kelime seçimi
+  wordArray = Array.from(word); // * Rastgele seçilen kelimeyi harf harf diziye aktarılması
+  wordNumber.innerHTML = wordArray.length; // * Kelime sayısını bildiren HTML elementine kelime sayısının yazdırılması
   addInput().then(() => {
-    const inputWord = document.querySelectorAll(".inputWord");
-    for (let index = 0; index < inputWord.length; index++) {
-      getWordCount.push(index);
+    // * addInput() fonksiyondaki işlem bittiken sonra çalışacak kodlar
+    const inputWord = document.querySelectorAll(".inputWord"); // * Tüm ".inputWord" class isimli elementlerin dizi halinde değişkene atanması
+    for (let index = 0; index < wordArray.length; index++) {
+      // * Kelime sayısı kadar çalışacak kod döngüsü
+      getWordCount.push(index); // * Harf alımı için kelime sayısı kadar diziye index eklenmesi
       inputWord[index].addEventListener("keyup", (e) => {
-        inputWord[index].value = inputWord[index].value.replace(/[^a-zA-ZğüşöçıİĞÜŞÖÇ]/g, "");
-        inputWord[index].value = inputWord[index].value.toLocaleUpperCase("tr-TR");
-        if (index + 1 < inputWord.length && e.key.length === 1 && /[a-zA-ZğüşöçıİĞÜŞÖÇ]/.test(e.key)) {
+        // * input elementlerine yazı yazıldığında çalışacak kodlar
+        inputWord[index].value = inputWord[index].value.replace(/[^a-zA-ZğüşöçıİĞÜŞÖÇ]/g, ""); // * input elementlerine sadece harf yazımı yazılabilmesi için
+        inputWord[index].value = inputWord[index].value.toLocaleUpperCase("tr-TR"); // * input elementlerindeki yazıları büyük harfe dömüştümek için
+        if (inputWord[index].value.trim() != "" && index + 1 < inputWord.length && e.key.length === 1 && /[a-zA-ZğüşöçıİĞÜŞÖÇ]/.test(e.key)) {
+          // * input elementine yazı yazıldığında sonraki input elementine geçiş için
           inputWord[index + 1].focus();
         } else if (index - 1 >= 0 && e.key == "Backspace") {
-          inputWord[index - 1].focus(() => {
-            inputWord[index - 1].select();
-          });
+          // * input elementinde yazı silindiğine önceki input elementine geçiş için
+          inputWord[index - 1].focus();
         }
       });
     }
   });
 
+  // * Kelimenin sözlük anlamı GET işlemi ve HTML DOM işlemi
   getRequest(`https://sozluk.gov.tr/gts?ara=${encodeURIComponent(word.toLocaleLowerCase("tr-TR"))}`)
     .then((response) => {
       tdk.innerHTML += `
@@ -58,11 +65,12 @@ const loadWord = () => {
     });
 };
 
+// * Kelime havuzuna ulaşmak için GET işlemi
 const getWords = () => {
   if (wordsDB.length == 0) {
     getRequest("/assets/json/words.json")
       .then((response) => {
-        wordsDB = response;
+        wordsDB = response; // * Kelime havuzunun değişkene atanması
         loadWord();
       })
       .catch((error) => {
@@ -73,37 +81,44 @@ const getWords = () => {
   }
 };
 
+// * Tahmin Et butonuna tıklandığında çalışacak kod işlemleri
 const submit = () => {
-  const inputWord = document.querySelectorAll(".inputWord");
+  const inputWord = document.querySelectorAll(".inputWord"); // * Tüm ".inputWord" class isimli elementlerin dizi halinde değişkene atanması
   let playerWord = "";
   for (let index = 0; index < inputWord.length; index++) {
+    // * Kelime sayısı kadar çalışacak kod döngüsü
     if (inputWord[index].value.trim() != "") {
-      playerWord += inputWord[index].value;
+      playerWord += inputWord[index].value; // * inputlardaki harfleri birleştirip kelimeye dönüştürme işlemi
     } else {
+      // * Eğer input boşsa...
       showStatusMsg(0);
       break;
     }
   }
 
   if (playerWord == word) {
+    // * Eğer "playerWord" rastgele üretilen kelime ise...
     showStatusMsg(1);
   } else if (playerWord != word && inputWord.length == playerWord.length) {
+    // * Eğer "playerWord" rastgele üretilen kelime değil ise...
     showStatusMsg(2);
   }
 };
 
+// * Harf Al butonuna tıklandığında çalışacak kod işlemleri
 const getWord = () => {
   const inputWord = document.querySelectorAll(".inputWord");
   if (getWordCount.length != 0) {
-    let getWordCountIndex = Math.floor(Math.random() * getWordCount.length);
-    let wordIndex = getWordCount[getWordCountIndex];
-    inputWord[wordIndex].value = wordArray[wordIndex];
-    inputWord[wordIndex].setAttribute("readonly", true);
-    inputWord[wordIndex].classList.add("readOnly");
-    getWordCount.splice(getWordCountIndex, getWordCountIndex == 0 ? 1 : 1);
+    let getWordCountIndex = Math.floor(Math.random() * getWordCount.length); // * "getWordCount" değişkeninden rastgele index alma
+    let wordIndex = getWordCount[getWordCountIndex]; // * "getWordCount" değişkeninden rastgele alınan index'in değişkendeki değer karşılığını alma
+    inputWord[wordIndex].value = wordArray[wordIndex]; // * Rastgele alınan harfi ilgili input'a yazdırma işlemi
+    inputWord[wordIndex].setAttribute("readonly", true); // * İlgili input'u düzenlenebilmesini engelleme işlemi
+    inputWord[wordIndex].classList.add("readOnly"); // * İlgili input'a class ekleme işlemi
+    getWordCount.splice(getWordCountIndex, getWordCountIndex == 0 ? 1 : 1); // * Rastgele alınan harf index'ini "getWordCount" dizisinden silme işlemi
   }
 };
 
+// * Cevap kontrol işlemleri
 const showStatusMsg = (statusCode) => {
   if (statusCode == 0) {
     statusMsgContentStatus.innerHTML = "Lütfen tüm harfleri doldurun";
